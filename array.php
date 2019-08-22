a5b69d447aeb95deeebd761962e1a674af460ed1
Narasimha183 <narasimhavenna4@gmail.com>
2019-08-22 12:08:27 +0530

    array file added

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+$students=array(array('mark',15,16),
+array('king',15,16),
+array('kohli',18,43));
+echo $students[0][0];
+echo $students[1][0];
+echo $students[2][0];
+echo $students[0][1];
+echo $students[1][1];
+echo $students[2][1];
+echo $students[0][2];
+echo $students[1][2];
+?>
+
